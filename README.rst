98bda383169950c81969f5be5e25f8f04dd26339
Tomasz Wysocki <tomasz@wysocki.info>
2012-09-06 15:56:39 +0200

    Start project.
    Change-Id: Ie0bf765df19b53d682693e7d8cb4cb730d2ac50a

---
+++ README.rst
@@ -0,0 +1,18 @@
+facebook-notifications
+======================
+
+Implements sending notifications to Facebook using Notifications API.
+
+https://developers.facebook.com/docs/app_notifications/
+
+
+Installation
+------------
+
+    $ pip install facebook-notifications
+
+
+Usage
+-------------
+
+TODO
---
+++ setup.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+from setuptools import find_packages
+from setuptools import setup
+
+def read(name):
+    from os import path
+    return open(path.join(path.dirname(__file__), name)).read()
+
+setup(
+    name='facebook-notifications',
+    version='0',
+    description="Sends notifications using Facebook Notification API",
+    long_description=read("README.rst"),
+    maintainer="Tomasz Wysocki",
+    maintainer_email="tomasz@wysocki.info",
+
+    install_requires=(
+        'facepy>=0.8.2',
+    ),
+    packages=find_packages(),
+)
