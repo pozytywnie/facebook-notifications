489bb7c39d0f5d081196d1b579fa77aaa6ba3ee3
Jan Chęć <jan@pozytywnie.pl>
2014-07-17 15:37:45 +0200

    Bump version number to 3.0.1.
    Change-Id: I06e81c2e16c94c768f50bf5e5c8e54f647879995

--- setup.py
+++ setup.py
@@ -2,13 +2,14 @@
 from setuptools import find_packages
 from setuptools import setup
 
+
 def read(name):
     from os import path
     return open(path.join(path.dirname(__file__), name)).read()
 
 setup(
     name='facebook-notifications',
-    version='3.0.0',
+    version='3.0.1',
     description="Sends notifications using Facebook Notification API",
     long_description=read("README.rst"),
     maintainer="Tomasz Wysocki",
